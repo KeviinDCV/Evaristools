90416a5058411e2a2da9980517968ac49ca34509
KeviinDCV <110347632+KeviinDCV@users.noreply.github.com>
2025-10-28 07:48:48 -0500

    Npm

--- public/build/assets/input-error-BosfKSjN.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-D0FZiB7O.js";import{c as o}from"./createLucideIcon-CzBdQ7kY.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
